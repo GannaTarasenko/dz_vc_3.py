ac86118a8cb896c01e6190272795581988128f1c
Hanna Tarasenko <maracheva.ganna@gmail.com>
2024-03-09 14:34:11 +0100

    2.1_cjv

--- dz_3.2.py
+++ dz_3.2.py
@@ -1,21 +1,16 @@
 import random
 
-def get_numbers_ticket(minimum, maximum, quantity):
-    # Перевірка коректності вхідних параметрів
-    if not (1 <= minimum <= maximum <= 1000) or not (1 <= quantity <= maximum - minimum + 1):
+def get_numbers_ticket(min, max, quantity):
+    if not (1 <= min <= max <= 1000) or not (1 <= quantity <= max - min + 1): #перевірка коректності вхідних параметрів
         return []
 
-    # Використання множини для забезпечення унікальності чисел
-    unique_numbers = set()
-
-    # Генерація випадкових унікальних чисел
+    unique_numbers = set() #створення порожньої множини
     while len(unique_numbers) < quantity:
-        unique_numbers.add(random.randint(minimum, maximum))
+        unique_numbers.add(random.randint(min, max)) #генерація випадкових унікальних чисел
 
-    # Повернення відсортованого списку унікальних чисел
-    return sorted(list(unique_numbers))
+    return sorted(list(unique_numbers)) #відсортований список унікальних чисел
 
 # Приклад використання
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(1, 1000, 9)
 print("Ваші лотерейні числа:", lottery_numbers)
 
