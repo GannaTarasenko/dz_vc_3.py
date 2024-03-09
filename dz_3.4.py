0f3218f20f5165b8c00f7a9f43e91b1fcc41da04
Hanna Tarasenko <maracheva.ganna@gmail.com>
2024-03-09 15:20:32 +0100

    3.4_com

--- dz_3.4.py
+++ dz_3.4.py
@@ -8,17 +8,13 @@ def get_upcoming_birthdays(users):
         birthday_date = datetime.strptime(user["birthday"], "%Y.%m.%d").date().replace(year=today.year)
 
         if birthday_date < today:
-            # День народження вже минув у цьому році, розглядаємо на наступний рік
-            birthday_date = birthday_date.replace(year=today.year + 1)
+            birthday_date = birthday_date.replace(year=today.year + 1) # День народження вже минув у цьому році, розглядаємо на наступний рік
 
-        # Визначаємо різницю між днем народження та поточним днем
-        days_until_birthday = (birthday_date - today).days
+        days_until_birthday = (birthday_date - today).days # Визначаємо різницю між днем народження та поточним днем
 
         if 0 <= days_until_birthday <= 7:
-            # Перевірка, чи день народження припадає на вихідний
-            if birthday_date.weekday() >= 5:
-                # Якщо припадає на вихідний, переносимо на наступний понеділок
-                birthday_date += timedelta(days=(7 - birthday_date.weekday()))
+            if birthday_date.weekday() >= 5: # Перевірка, чи день народження припадає на вихідний
+                birthday_date += timedelta(days=(7 - birthday_date.weekday())) # Якщо припадає на вихідний, переносимо на наступний понеділок
 
             upcoming_birthdays.append({
                 'name': user['name'],
@@ -29,8 +25,10 @@ def get_upcoming_birthdays(users):
 
 # Приклад використання
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
-    {"name": "Jane Smith", "birthday": "1990.01.27"}
+    {"name": "Надія", "birthday": "2016.03.10"},
+    {"name": "Свят", "birthday": "2018.03.12"},
+    {"name": "Вова", "birthday": "1986.03.03"},
+    {"name": "Ганна", "birthday": "1988.03.21"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
